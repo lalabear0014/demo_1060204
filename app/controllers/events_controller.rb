add964a6e3ad9abe28e2727936be229e9694b44c
Air <air@Airs-MacBook.local>
2017-02-04 15:01:24 +0800

    Add new & create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+	# 注意！！！只有 index 的 @events —> 有加s
+	# 其他的 action 的 @event 都沒有
+
+	# GET /events/index
+	# GET /events
+	def index
+		@events = Event.all
+	end
+
+	# GET /events/new
+	def new
+		@event = Event.new
+	end
+
+	# POST /events/create
+	def create
+		@event = Event.new( params[:event] )
+	end
 end
