9fbaf88e33f2fe3d8af0c0564d659adf6422d411
Air <air@Airs-MacBook.local>
2017-02-04 22:02:26 +0800

    Add destroy action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -40,6 +40,14 @@ class EventsController < ApplicationController
 		redirect_to :action => :show, :id => @event
 	end
 
+	# GET /events/destroy/:id
+	def destroy
+		@event = Event.find( params[:id] )
+		@event.destroy
+
+		redirect_to :action => :index
+	end
+
 	private
 
 	def event_params
