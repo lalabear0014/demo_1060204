62641946f045c994d705f7cd7ee6515a032bcb13
Air <air@Airs-MacBook.local>
2017-02-05 20:10:08 +0800

    Generate model attendee & db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170204060037) do
+ActiveRecord::Schema.define(version: 20170205120233) do
+
+  create_table "attendees", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_attendees_on_event_id"
+  end
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
