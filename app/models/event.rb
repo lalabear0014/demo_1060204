2017e39b40f781743a9552ae91f5efd12f2d1f7f
Air <air@Airs-MacBook.local>
2017-02-06 13:51:17 +0800

    Generate model group&groupship db:migrate

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,7 @@ class Event < ApplicationRecord
 
 	delegate :name, :to => :category, :prefix => true, :allow_nil => true
 	
+	has_many :event_groupships
+	has_many :groups, :through => :event_groupships
+
 end
