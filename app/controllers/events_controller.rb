f9d53e70ddd92156e5f55c631fe31779a506263c
Air <air@Airs-MacBook.local>
2017-02-06 14:16:20 +0800

    Add checkboxes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -67,7 +67,7 @@ class EventsController < ApplicationController
 	end
 
 	def event_params
-		params.require(:event).permit(:name, :description, :category_id)
+		params.require(:event).permit(:name, :description, :category_id, :group_ids => [])
 	end
 
 
